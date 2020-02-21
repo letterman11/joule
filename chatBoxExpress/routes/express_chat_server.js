9eab398063bf70ab32721e7c9d1f4a98c48bd388
angus <angus>
2020-02-21 01:01:00 +0000

    update of chat server largely finished express nodejs version functional with the need of multiple connections to database in a given query

--- chatBoxExpress/routes/express_chat_server.js
+++ chatBoxExpress/routes/express_chat_server.js
@@ -16,37 +16,52 @@ function createConnectDB() {
       database : 'dcoda_acme'
     });
 
-    return connection;
+  return connection;
 }
 
+function genTimeStamp() {
+
+  //2020-2-20 13:13:58
+  var ts = new Date()
+  var time_str = ts.toTimeString().substr(0,9);
+  var year = ts.getFullYear();
+  var month = ts.getUTCMonth()+1;
+  var day = ts.getDate();
+  var timeStamp = year +"-"+month+"-"+day+" "+time_str;
+  return timeStamp; 
+ 
+}
 
 /* ------------------------------------------------------------------
 | roomIDs              (1)
 | load room names
+| the default state of application for a logged in user
 |-------------------------------------------------------------------*/
 
 var sqlstr_sel_chatroom = "select room_id from dcoda_acme.chat_room";
 router.get('/', function(req, res) {
 
-  connection = createConnectDB();
+  var conn = createConnectDB();
   var roomIdArray = [];
+
   console.log("Get chat server Successful Route");
   if(req.query.req == "roomIDs") 
   {
-   	 connection.query(sqlstr_sel_chatroom, function (error, results, fields) {
+   	 conn.query(sqlstr_sel_chatroom, function (error, room_results, fields) {
 	     if (error) throw error;
-  	     console.log('The room IDs: ', results);
-	     results.forEach(function(elem) {
+  	     console.log('The room IDs: ', room_results, room_results.length, fields);
+	     room_results.forEach(function(elem) {
           roomIdArray.push(elem.room_id);
-          console.log(roomIdArray);
+          //console.log(roomIdArray);
          });
 
-       console.log("bank ", roomIdArray);
+       //console.log("bank ", roomIdArray);
        res.json(roomIdArray);
-    });
+   });
     
   }
-  connection.end();
+
+  conn.end();
 
 });
 
@@ -54,32 +69,39 @@ router.get('/', function(req, res) {
 | roomLogin         (2)
 |
 | insert of client into link table +user_cr+ to associate client 
-| with a chat room   thus creating an entry in the chatroom queue table
+| with a chat room thus creating an entry in the chatroom queue table
 |-----------------------------------------------------------------*/
 
-var sqlstr1_insert_user_cr = "insert into user_cr values (?, ?, ?)";
-var sqlstr1_update_user_cr = "update user_cr set  user_id = ?, room_id = ?, date_ts = ?, room_name = ?, where user_id = ?";
+var insert_sqlstr1_user_cr = "insert into user_cr values (?, ?, ?, ?)";
+var update_sqlstr1_user_cr = "update user_cr set user_id = ?, room_id = ?, date_ts = ?, room_name = ? where user_id = ?";
 
-router.post('/roomLogin', function(req, res, next) {
+router.post('/roomLogin', function(req, res) {
   console.log("Post RoomLogin chat server Successful Route");
 
-    connection.query(sqlstr1_sel_chatroom, [q_user_id, q_room_id, q_now_str, q_room_id ], function (error, results, fields) {
-     if (error) throw error;
-     
-     console.log('rooms: ', results);
-    });
+  var conn = createConnectDB();
 
-    //if fail update instead of insert
-       connection.query(sqlstr1_update_user_cr, [ q_user_id, q_room_id, q_date_ts, q_room_name, q_user_id], function (error, results, fields) {
-          if (error) throw error;
-       console.log('rooms: ', results);
-      });
+  var q_user_id = req.body.userID;
+  var q_room_id = req.body.roomID;
+  var q_room_name = q_room_id;
+  var q_now_str = genTimeStamp();
 
-	//   else success
-         var sqlstr2_sel_user_cr = "select user_id from user_cr where room_id = ? " ;
-        connection.query(sqlstr2_sel_user_cr, function (error, results, fields) { if (error) throw error;
-       console.log('rooms: ', results);
-     });
+  conn.query(insert_sqlstr1_user_cr, [q_user_id, q_room_id, q_now_str, 
+					q_room_name ], function (error, results, fields) {
+         if (error) {
+            console.log('Error message: ', error.sqlMessage);
+            var conn2 = createConnectDB();
+            conn2.query(update_sqlstr1_user_cr, [ q_user_id, q_room_id, q_now_str, 
+						q_room_name, q_user_id], function (error, results, fields) {
+             if (error) throw error;
+               console.log('Rows changed for update: ', results.changedRows);
+    	    });
+			conn2.end();
+         } 
+         console.log('Rows affected for insert: ', results);
+   }); 
+  
+  conn.end();  
+  res.send("OK");
 });
 
 /*----------------------------------------------------------------
@@ -88,21 +110,21 @@ router.post('/roomLogin', function(req, res, next) {
 |  delete from table +user_cr+ for the logged out user
 |  send back status
 ----------------------------------------------------------------*/
-var sqlstr1_del_user_cr = "delete from user_cr where user_id = ? ";
+var delete_sqlstr1_user_cr = "delete from user_cr where user_id = ? ";
 
-router.post('/roomLogout', function(req, res, next) {
-  console.log("Post RoomLogout chat server Successful Route");
+router.post('/roomLogout', function(req, res) {
 
+  var q_user_id = req.body.userID;
+  var conn = createConnectDB();
 
-    connection.query(sqlstr1_del_user_cr, [q_user_id], function (error, results, fields) {
+    conn.query(delete_sqlstr1_user_cr, [q_user_id], function (error, results, fields) {
      if (error) throw error;
-     console.log('rooms: ', results);
+     console.log('rows affected for deletion: ', results.affectedRows);
     });
 
+  conn.end();
 
-
-
-
+  res.send("OK");
 });
 
 /* ------------------------------------------------------------------
@@ -116,32 +138,45 @@ router.post('/roomLogout', function(req, res, next) {
 | JSON
 | DONE -4
 -----------------------------------------------------------------------*/
-var sqlstr1_sel_send_user_cr = "select user_id from user_cr where room_id = ? " ;
-var sqlstr_insert_chatroom_queue = "insert into chat_room_queue (user_id, room_id, insert_ts, chat_text, msg_user_id) values (?,?,?,?,?) ";
+var select_sqlstr1_send_user_cr = "select user_id from user_cr where room_id = ? " ;
+var insert_sqlstr1_chatroom_queue = "insert into chat_room_queue (user_id, room_id, insert_ts, chat_text, msg_user_id) values (?,?,?,?,?) ";
 
 router.post('/sendMsg', function(req, res, next) {
-
   console.log("Post Send Message chat server Successful Route");
 
+   var q_user_id = req.body.userID;
+   var q_room_id = req.body.roomID;
+   var q_chat_text = req.body.msgText;
+   var q_insert_ts = genTimeStamp();
+   var q_msg_user_id;
+   var conn = createConnectDB();
+   var conn2 = createConnectDB();
 
-   connection.query(sqlstr1_sel_send_user_cr, [q_room_id], function (error, results, fields) {
+   console.log("------------------------------", q_user_id, q_room_id, q_chat_text,q_insert_ts);
+   conn.query(select_sqlstr1_send_user_cr, [q_room_id], function (error, msg_user_array, fields) {
+
     if (error) throw error;
-    console.log('rooms: ', results);
-   });
+      
+      for(var i=0; i <msg_user_array.length; i++) {
+         q_msg_user_id = msg_user_array[i].user_id;
+         conn2.query(insert_sqlstr1_chatroom_queue, [q_user_id, q_room_id, q_insert_ts, 
+				q_chat_text, q_msg_user_id], function (error, results, fields) {
+          if (error) throw error;
+          console.log('chat text for all users in given room: ', results);
 
+         }); // end insert
+      }
+      conn2.end();
+   }); //end org select
 
-    connection.query(sqlstr1_insert_chatroom_queue, [q_user_id, q_room_id, q_insert_ts, q_chat_text, q_mag_user_id], function (error, results, fields) {
-     if (error) throw error;
-     CONSOle.log('rooms: ', results);
-    });
-
+   conn.end(); 
+   res.send("OK");
 });
 
 /* ------------------------------------------------------------------
-| Error    (5)
+| Invalid Session     (5)
 |
 |
-| An error occurred report back to client     
 | DONE -5
 |-------------------------------------------------------------------*/
 
