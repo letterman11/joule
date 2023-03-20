f27fb3547899f5be01f6268d52720152edcccf8a
Angus Brooks <angusbrooks@gmail.com>
2023-03-20 10:44:04 -0400

    add of data def of expressChat

---
+++ chatBoxExpress/data/populateChatRoom.sql
@@ -0,0 +1,8 @@
+use dcoda_acme;
+
+
+insert into chat_room values("java","java", now());
+insert into chat_room values("finance","finance", now());
+insert into chat_room values("basketball","basketball", now());
+insert into chat_room values("soccer","soccer", now());
+
