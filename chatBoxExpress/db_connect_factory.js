9d07b6bc9317dfeeb4915a2dcb3a8ef01877eccf
Angus Brooks <angusbrooks@gmail.com>
2025-12-20 18:37:30 -0500

    database parameters now parsed from ini file

--- chatBoxExpress/db_connect_factory.js
+++ chatBoxExpress/db_connect_factory.js
@@ -4,29 +4,62 @@
 -------------*/
 
 const mysql = require('mysql');
+const fs = require('fs');
+
+const fileConfig = 'stockDbConfig.dat';
+const regEx = /([A-Za-z_0-9]+)=([A-Za-z_0-9\*\-\/\.\*\:\\]+)/;
+var rd = new Map(); 
+
+readDbConfig()
 
 var DbConfig = {
 
-     host 		: "localhost",
-     user 		: "dococt",
-     password	: "dococt",
-     database 	: "dcoda_acme",
+     host 		: rd.get('hostname'),
+     user 		: rd.get('user'),
+     password	: rd.get('password'),
+     database 	: rd.get('database'),
+     port       : rd.get('port'),
 
 
 
     createConnectDB: function () {
    
      connection = mysql.createConnection({
-      host     : this.host,
-      user     : this.user,
-      password : this.password,
-      database : this.database
+     host       : this.host,
+     user       : this.user,
+     password   : this.password,
+     database   : this.database,
+     port       : this.port
     });
 
   return connection;
    }
 
 };
+
+function readDbConfig()
+{
+
+    try {
+      // Specify 'utf8' encoding to get a string
+        const data = fs.readFileSync(fileConfig, 'utf8');
+        arr_lines = data.split(/\n/)  
+        
+        for (var i=0;  i < arr_lines.length; i++) {
+            if ( arr_lines[i].match(/^[#|\s]+/))
+                continue
+
+            var match =  arr_lines[i].match(regEx);
+            if (match)
+                rd.set(match[1], match[2])     
+            
+        }
+      
+    } catch (err) {
+      console.error('Error reading file:', err);
+    }
+
+}
 
 
 module.exports = DbConfig
